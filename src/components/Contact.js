c7a13bee16df6b69c942b39d139183b822dd5ee9
muhammadrezaa21 <mreza21031998@gmail.com>
2022-06-20 15:14:33 +0700

    membuat header dan contact componen

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,18 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = ({ data }) => {
+  return (
+    <div className="contact">
+      <img src={data.photo} className="contact-image" />
+      <div className="contact-info">
+        <p className="contact-name">{data.name}</p>
+        <p className="contact-phone">{data.phone}</p>
+        <p className="contact-email">{data.email}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
